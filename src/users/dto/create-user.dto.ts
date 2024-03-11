36b0e2a49daa5e71b524e372119414324989d60b
Adiar <93129221+Okarix@users.noreply.github.com>
2024-03-11 22:21:03 +0500

    feat: user auth(login)

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ default: 'test@gmail.com' })
   email: string;
+
+  @ApiProperty({ default: 'Tester Tester' })
   fullName: string;
+
+  @ApiProperty({ default: '12345678' })
   password: string;
 }
